59630d2475afc1e9e7b3cf49337671fd3ca96fec
Tannaz <tanaz@mystage.ca>
2023-06-23 20:39:46 +0430

    nav-draft

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div className="bg-orange-600 min-w-full">
+      <div className="text-left">Nav</div>
+      <h1 className="text-center">hello</h1>
+    </div>
+  );
+}
+
+export default Nav;
