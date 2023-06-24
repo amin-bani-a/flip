91d242fb37b89f7793c2cf71b3f1e1cdc1a0266e
Tannaz <tanaz@mystage.ca>
2023-06-24 15:07:35 +0400

    nav-first-draft

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Header() {
     return (
-        <div className='bg-red-600 text-center h-40 justify-center'>Header</div>
+        <div className='bg-red-600 text-center h-[6.75rem] flex flex-row justify-between items-center'>
+            <div className='bg-yellow-500 w-15 text-5xl m-1 p-'>FLIP TURN</div>
+            <div>
+                <div className='bg-red-700 w-10 m-1 p-1 '>Nav</div>
+            </div>
+        </div>
     )
 }
 
